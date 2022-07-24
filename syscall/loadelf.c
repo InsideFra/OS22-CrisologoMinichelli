f76f00a4b14d4dc871c432af2621945c0de31152
Francesco Minichelli <francescophp@gmail.com>
2022-07-24 14:12:25 +0200

    Small changes for DEBUG

--- syscall/loadelf.c
+++ syscall/loadelf.c
@@ -90,7 +90,7 @@ load_segment(struct addrspace *as, struct vnode *v,
 		filesize = memsize;
 	}
 
-	DEBUG(DB_EXEC, "ELF: Loading %lu bytes to 0x%lx\n",
+	DEBUG(DB_VM, "ELF: Loading %lu bytes to 0x%lx\n",
 	      (unsigned long) filesize, (unsigned long) vaddr);
 
 	iov.iov_ubase = (userptr_t)vaddr;
