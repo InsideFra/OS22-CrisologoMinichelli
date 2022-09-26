915ee95945ab3d88dbd8b53dcad9ddf097d9f359
Marco Crisologo <258146@studenti.unimore.it>
2022-09-26 10:06:40 +0000

    TLB_remove changed

--- vm/vm_tlb.c
+++ vm/vm_tlb.c
@@ -8,93 +8,108 @@
 #include <pt.h>
 
 extern unsigned int PAGETABLE_ENTRY;
-extern struct invertedPT (*main_PG);
+extern struct invertedPT(*main_PG);
 uint32_t freeTLBEntries = 0;
+uint32_t TLB_victim;
 /**
-* This method is used to add an entry to the hardware TLB.
-*
-* The physical address is taken from the page table. 
-* If the Virtual Address and PID is not in the TLB, the kernel crash
-*
-* @author @InsideFra
-* @param vaddr The virtual address (0x00..) (even not aligned)
-* @param pid The process pid
-* @param Dirty Specify if the segment is writable (== 1) or not (== 0)
-* @date 02/08/2022
-* @return 0 if everything is okay else panic
-*/
-int
-addTLB(vaddr_t vaddr, pid_t pid, _Bool Dirty) {
+ * This method is used to add an entry to the hardware TLB.
+ *
+ * The physical address is taken from the page table.
+ * If the Virtual Address and PID is not in the TLB, the kernel crash
+ *
+ * @author @InsideFra
+ * @param vaddr The virtual address (0x00..) (even not aligned)
+ * @param pid The process pid
+ * @param Dirty Specify if the segment is writable (== 1) or not (== 0)
+ * @date 02/08/2022
+ * @return 0 if everything is okay else panic
+ */
+int addTLB(vaddr_t vaddr, pid_t pid, _Bool Dirty)
+{
     uint32_t ehi, elo;
     paddr_t pa, paddr = 0;
     int32_t tlb_index_probe;
-    uint32_t page_index = vaddr/PAGE_SIZE;
+    uint32_t page_index = vaddr / PAGE_SIZE;
 
-    for (unsigned int i = 0; i < PAGETABLE_ENTRY; i++) {
+    for (unsigned int i = 0; i < PAGETABLE_ENTRY; i++)
+    {
         if (main_PG[i].Valid == 0)
             continue;
-        
+
         if (main_PG[i].page_number == page_index)
-            if(main_PG[i].pid == pid) {
-                paddr = i*PAGE_SIZE + MIPS_KSEG0;
+            if (main_PG[i].pid == pid)
+            {
+                paddr = i * PAGE_SIZE + MIPS_KSEG0;
             }
     }
 
-    if (paddr == 0) {
+    if (paddr == 0)
+    {
         panic("Error in addTLB from pageSearch");
         print_page_table();
     }
 
     ehi = vaddr & PAGE_FRAME; // PAGE ALIGN
     pa = paddr - MIPS_KSEG0;
-    if (Dirty) 
+    if (Dirty)
         elo = pa | TLBLO_VALID | TLBLO_DIRTY;
     else
         elo = pa | TLBLO_VALID;
 
     int32_t spl = splhigh();
-    tlb_index_probe = tlb_probe(ehi, elo); 
-    if (tlb_index_probe > 0) {
+    tlb_index_probe = tlb_probe(ehi, elo);
+    if (tlb_index_probe > 0)
+    {
         panic("TLB Error: duplicate TLB entries\n");
         return 1;
     }
 
-    for (unsigned int i = 0; i < NUM_TLB; i++) {
+    for (unsigned int i = 0; i < NUM_TLB; i++)
+    {
         tlb_read(&ehi, &elo, i);
 
-        if(! ((elo & 0x00000fff) & TLBLO_VALID)) { // invalid
+        if (!((elo & 0x00000fff) & TLBLO_VALID))
+        {                             // invalid
             ehi = vaddr & PAGE_FRAME; // PAGE ALIGN
             pa = paddr - MIPS_KSEG0;
             elo = pa | TLBLO_VALID | TLBLO_DIRTY;
             tlb_write(ehi, elo, i);
 
-            tlb_index_probe = tlb_probe(ehi, elo); 
-            if (tlb_index_probe < 0) {
+            tlb_index_probe = tlb_probe(ehi, elo);
+            if (tlb_index_probe < 0)
+            {
                 panic("Generic TLB Error\n");
             }
 
             DEBUG(DB_TLB, "(TLBwrite ): [%3d] PN: %x\tpAddr: 0x%x\n", tlb_index_probe, ehi >> 12, paddr);
 
-            freeTLBEntries--;;
+            freeTLBEntries--;
+            ;
 
             splx(spl);
             return 0;
         }
     }
-     
+
+    /*---------------------------- TLB ROUND ROBIN REPLACEMENT --------------------------------*/
+
     ehi = vaddr & PAGE_FRAME; // PAGE ALIGN
     pa = paddr - MIPS_KSEG0;
     elo = pa | TLBLO_VALID | TLBLO_DIRTY;
-    tlb_random(ehi, elo);
 
-    tlb_index_probe = tlb_probe(ehi, elo); 
-    if (tlb_index_probe < 0) {
+    TLB_victim = tlb_get_rr_victim();
+    tlb_write(ehi, elo, TLB_victim);
+
+    tlb_index_probe = tlb_probe(ehi, elo);
+    if (tlb_index_probe < 0)
+    {
         panic("Generic TLB Error\n");
     }
 
     DEBUG(DB_TLB, "(TLBreplac): [%3d] PN: %x\tpAddr: 0x%x\n", tlb_index_probe, ehi >> 12, paddr);
 
-    freeTLBEntries--;;
+    freeTLBEntries--;
+    ;
 
     splx(spl);
     return 0;
@@ -104,23 +119,25 @@ addTLB(vaddr_t vaddr, pid_t pid, _Bool Dirty) {
     return 1;
 }
 /**
-* This method is used to remove an entry to the hardware TLB.
-* @author @InsideFra
-* @date 02/08/2022
-* @param {vaddr_t} vaddr The virtual address (0x00..) (even not aligned)
-* @return 0 if everything is entry is missing/the entry has been removed, 1 if error
-*/
-int
-removeTLB(vaddr_t vaddr) {
+ * This method is used to remove an entry to the hardware TLB.
+ * @author @InsideFra
+ * @date 02/08/2022
+ * @param {vaddr_t} vaddr The virtual address (0x00..) (even not aligned)
+ * @return 0 if everything is entry is missing/the entry has been removed, 1 if error
+ */
+int removeTLB(vaddr_t vaddr)
+{
     uint32_t ehi, elo;
     uint32_t paddr;
     vaddr &= PAGE_FRAME; // PAGE ALIGN
 
     uint32_t spl = splhigh();
-    for (unsigned int i = 0; i < NUM_TLB; i++) {
+    for (unsigned int i = 0; i < NUM_TLB; i++)
+    {
         tlb_read(&ehi, &elo, i);
 
-        if(ehi == vaddr) {
+        if (ehi == vaddr)
+        {
             paddr = elo >> 12;
             DEBUG(DB_TLB, "(TLBremove): [%3d] PN: %x\tpAddr: 0x%x\t", i, vaddr >> 12, PADDR_TO_KVADDR(paddr));
             freeTLBEntries++;
@@ -128,19 +145,27 @@ removeTLB(vaddr_t vaddr) {
             ehi = TLBHI_INVALID(i);
             elo = TLBLO_INVALID();
             tlb_write(ehi, elo, i);
-            
-            if( (elo & 0x00000fff) & TLBLO_VALID) {
-                DEBUG(DB_TLB, "-N- "); }
-            else {
-                DEBUG(DB_TLB, "-V- "); }
 
-            if( (elo & 0x00000fff) & TLBLO_DIRTY) {
-                DEBUG(DB_TLB, "D   -"); }
-            else {
-                DEBUG(DB_TLB, "-ND  -"); }
-            
+            if ((elo & 0x00000fff) & TLBLO_VALID)
+            {
+                DEBUG(DB_TLB, "-N- ");
+            }
+            else
+            {
+                DEBUG(DB_TLB, "-V- ");
+            }
+
+            if ((elo & 0x00000fff) & TLBLO_DIRTY)
+            {
+                DEBUG(DB_TLB, "D   -");
+            }
+            else
+            {
+                DEBUG(DB_TLB, "-ND  -");
+            }
+
             DEBUG(DB_TLB, "\n");
-            
+
             splx(spl);
             return 0;
         }
@@ -148,4 +173,36 @@ removeTLB(vaddr_t vaddr) {
     DEBUG(DB_TLB, "removeTLB: noEntryFound\t(vAddr: 0x%x)\n", (uint32_t)vaddr);
     splx(spl);
     return 0;
+}
+
+int removeTLBv1(vaddr_t vaddr)
+{
+    vaddr &= PAGE_FRAME; // PAGE ALIGN
+    uint32_t ehi = vaddr;
+    uint32_t elo;
+    int probe_result = 0;
+    uint32_t spl = splhigh();
+
+    probe_result = tlb_probe(ehi, 0);
+    if(probe_result == noEntryFound){
+        DEBUG(DB_TLB, "removeTLB: noEntryFound\t(vAddr: 0x%x)\n", (uint32_t)vaddr);
+        splx(spl);
+        return 0;
+    }
+    ehi = TLBHI_INVALID(probe_result);
+    elo = TLBLO_INVALID();
+    tlb_write(ehi, elo, probe_result);
+    freeTLBEntries++;
+    splx(spl);
+    return 0;
+
+}
+
+int tlb_get_rr_victim(void)
+{
+    int victim;
+    static unsigned int next_victim = 0;
+    victim = next_victim;
+    next_victim = (next_victim + 1) % NUM_TLB;
+    return victim;
 }
